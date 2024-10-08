2034c5e0f4533cb594b4d3afc0773287027f9bf4
ritabratamoitra <ritabrata1808@live.com>
2024-10-08 07:58:59 +0530

    Add conversion stats

--- src/pages/2 - Analytics.py
+++ src/pages/2 - Analytics.py
@@ -6,7 +6,7 @@ from core.io import read_main_data
 st.title("Lead Insights")
 data = read_main_data()
 
-with st.expander("Overall Demographics"):
+with st.expander("Overall Demographics", expanded=True):
     st.bar_chart(
         data['Location'].value_counts(), x_label="City", y_label="# Leads"
     )
@@ -23,3 +23,24 @@ with st.expander("Overall Demographics"):
         title="% Conversions",
     )
     st.plotly_chart(fig, theme=None)
+
+with st.expander("Conversion Stats", expanded=True):
+    conv_loc = data \
+        .groupby('Location')['Conversion (Target)'].sum().reset_index()
+
+    fig = px.pie(
+        conv_loc, values='Conversion (Target)', names='Location', hole=.3,
+        title="Conversion percentage by Location"
+    )
+    st.plotly_chart(fig, theme=None)
+
+    status_loc = data.groupby('LeadStatus').agg(
+        {'Conversion (Target)': 'sum'}
+    ).reset_index()
+
+    st.bar_chart(
+        status_loc,
+        x='LeadStatus',
+        y='Conversion (Target)',
+        x_label="LeadStatus", y_label="# Conversions", color='#ffaa00'
+    )
